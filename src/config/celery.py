cf44854afd37804458a3710fcc79f68c69cea22f
Vitor Moreira <viitornmoreira@outlook.com>
2025-09-07 19:21:41 -0300

    feat: update celery configuration to include beat schedule for periodic scraping

--- src/config/celery.py
+++ src/config/celery.py
@@ -6,12 +6,18 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('config')
 
-
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
-
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
+
+app.conf.beat_schedule = {
+    'periodic-scraping': {
+        'task': 'scraping.tasks.periodic_scraping',
+        'schedule': 3600.0,
+    },
+}
+app.conf.timezone = 'UTC'
